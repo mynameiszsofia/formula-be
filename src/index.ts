57e2b8e9296c6e5a13418eb9933f41d0656f526b
mynameiszsofia <lakatos.zsofia1993@gmail.com>
2022-10-22 10:48:49 +0200

    Init app

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express()
+const port = Number(process.env.PORT ?? 8080)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Formula app listening on port ${port}`)
+})
